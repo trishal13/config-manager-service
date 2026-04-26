3e7b91e688d95706dd7ca99cd8ec3c5604e88931
Trishal <trishalmakhija@gmail.com>
2026-04-26 22:42:17 +0530

    Initial commit

---
+++ src/main/java/com/projects/config_manager_service/ConfigManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projects.config_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/config_manager_service/ConfigManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.config_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
